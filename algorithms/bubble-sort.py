7f57fae07910ab1f5400bee97021b48b8a007327
ParigiJ <135647142+ParigiJ@users.noreply.github.com>
2025-12-29 11:41:53 -0600

    Add Bubble Sort to algorithms

--- algorithms/bubble-sort.py
+++ algorithms/bubble-sort.py
@@ -1,4 +1,19 @@
 def bubbleSort(nums: list) -> list:
+    """
+    Bubble sort algorithm.
+
+    :param nums: List of integers to be sorted
+    :type nums: list
+    :return: Sorted list of integers
+    :rtype: list
+    """
+    """
+    This algorithm takes a list as an input
+    it starts comparing the first element with next element
+    then if the element next to it is smaller than the current element it is swapped and 
+    the process is repeated and the last element will be the largest element
+    and then the range decreases as list is sorted"""
+
     for i in range(0, len(nums) - 1):
         swapped = False
 
